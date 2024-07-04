f305f0db1c37af9c49add9934f4ef367b22c19ec
AlbertjRR <albert_jr001@hotmail.com>
2024-07-04 17:11:04 -0400

    Update AlbertRodriguez_2.cpp

--- AlbertRodriguez_2.cpp
+++ AlbertRodriguez_2.cpp
@@ -1,29 +1,26 @@
-        //Ejercicio 2
-
-
-
+//Ejercicio num 2
 
 #include <iostream>
 #include <fstream>
 
 int main() {
-    int cantidadSueldos;
+    int cantSueldo;
 
-    std::cout << "Por favor David ingrese la cantidad de sueldos a almacenar: ";
-    std::cin >> cantidadSueldos;
+    std::cout << "ingresar los sueldos para almacenar: ";
+    std::cin >> cantSueldo;
 
-    std::ofstream archivo("sueldos.dat", std::ios::binary); 
+    std::ofstream archivo("sueldos", std::ios::binary); 
 
     if (archivo.is_open()) {
         float sueldo;
 
-        for (int i = 0; i < cantidadSueldos; i++) {
-            std::cout << "David por favor Ingresa el sueldo del trabajor  " << i + 1 << ": ";
+        for (int i = 0; i < cantSueldo; i++) {
+            std::cout << "Ingrese el sueldo del trabajor  " << i + 1 << ": ";
             std::cin >> sueldo;
             archivo.write(reinterpret_cast<const char*>(&sueldo), sizeof(float)); 
         }
 
-        std::cout << "Los sueldos fueron almacenados correctamente en el archivo sueldos.dat." << std::endl;
+        std::cout << "Los sueldos fueron almacenados correctamente en el archivo sueldos." << std::endl;
     } else {
         std::cerr << "Error al crear el archivo." << std::endl;
     }
